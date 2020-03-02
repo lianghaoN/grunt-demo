87df86e8f0d61fe2fc79a0e8873306f94f0fe264
lianghao <597868880@qq.com>
2020-03-02 23:24:35 +0800

    http-api-test

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,22 @@
+/*global module:true */
 module.exports = function (grunt) {
-  grunt.initConfig({
-    mocha: {
-      test: {
-        rec: ['test/index.html'] 
-      },
-      options:{
-        run: true,
-        reportor: 'Dot'
-      }
-    }
-  });
+    grunt.initConfig({
+        run: {
+            api: {
+                options: { wait: false },
+                args: ['./app.js']
+            }
+        },
+    mochacli :{
+            all:['test/*.js'],
+            options:{
+                reportor:'spec',
+                bail:true
+            }
+        }
+    });
 
-  grunt.loadNpmTasks('grunt-mocha');
-
-  grunt.registerTask('default', ['mocha']);
+    grunt.loadNpmTasks('grunt-run');
+    grunt.loadNpmTasks('grunt-mocha-cli');
+    grunt.registerTask('default', ['run', 'mochacli', 'stop:api']);
 };
